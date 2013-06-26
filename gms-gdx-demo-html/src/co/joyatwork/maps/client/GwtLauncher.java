7cdc49ae2a1ff17e5755ee7927d0b39a040a9b09
ihla <lubos.ilcik@gmail.com>
2013-06-26 10:46:30 +0200

    - init

---
+++ gms-gdx-demo-html/src/co/joyatwork/maps/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package co.joyatwork.maps.client;
+
+import co.joyatwork.maps.GmsGdxDemo;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new GmsGdxDemo();
+	}
+}
