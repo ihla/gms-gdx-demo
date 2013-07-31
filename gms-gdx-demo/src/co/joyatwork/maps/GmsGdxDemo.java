dc0a371bd190f1de5600b4b57fd3604880f6139c
ihla <lubos.ilcik@gmail.com>
2013-07-31 13:53:40 +0200

    - +

--- gms-gdx-demo/src/co/joyatwork/maps/GmsGdxDemo.java
+++ gms-gdx-demo/src/co/joyatwork/maps/GmsGdxDemo.java
@@ -74,6 +74,7 @@ public class GmsGdxDemo implements ApplicationListener {
 		camController.update();
 
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
  
         modelBatch.begin(cam);
